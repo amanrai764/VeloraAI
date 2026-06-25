32059e8a76514360f5c2329d2a99fc842ed56ceb
amanrai <raiaman01033@gmail.com>
2026-06-25 12:54:45 +0530

    ab frontend setup karlo thoda

--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,17 @@
 import express from "express"//we are using type:module so we can use import export
 import dotenv from "dotenv"
 import connectDB from "./Configs/ConnectDB.js"
+import authRouter from "./Routes/auth.route.js"
+import cookieParser from "cookie-parser"
 dotenv.config()
 const app=express()
-const PORT=process.env.PORT
+app.use(express.json())
+app.use(cookieParser())
 app.get("/",(req,res)=>{
     res.json("Hello from server")
 })
+app.use("/api/auth",authRouter)
+const PORT=process.env.PORT
 app.listen(PORT,()=>{
     console.log(`Server Started on port ${PORT}`)
     connectDB()
