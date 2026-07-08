79b0ad7db06088cf3e32a2da5257abb5751448f7
amanrai <raiaman01033@gmail.com>
2026-07-08 12:49:22 +0530

    Add Tailwind dependencies

--- Server/Configs/token.js
+++ Server/Configs/token.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken"
-
+//fixing all issues
 export const genToken = async (userId) => {
     try {
         const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "7d" })
